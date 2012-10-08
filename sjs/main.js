76566d84ce9c7ee07b4f52ae2d542ef61cad3096
James Tomasino <james@tomasino.org>
2012-10-08 17:15:37 -0400

    handlebars templates added and pulling content from json

--- sjs/main.js
+++ sjs/main.js
@@ -1,4 +1,5 @@
 //= require lib/jquery-1.8.2
+//= require lib/handlebars-1.0.rc.1
 //= require lib/l10n
 //= require lib/flickr
 
@@ -6,10 +7,35 @@ var flickr = new Flickr();
 flickr.showRandomPicture( onFlickrImage );
 
 function onFlickrImage ( obj ){
-	$('.background_image').css('background-image', 'url(' + obj.url + ')');
-	$('.background_image').css('filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + obj.url + '", sizingMethod="scale")');
-	$('.background-image').css('-ms-filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + obj.url + '", sizingMethod="scale")');
-	$('.background_image').css('background-repeat', 'no-repeat');
-	$('.background_image').css('background-position', 'center center');
-	$('.background_image').css('background-attachment', 'fixed');
+	$('.flickr').css('background-image', 'url(' + obj.url + ')');
+	$('.flickr').css('filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + obj.url + '", sizingMethod="scale")');
+	$('.flickr').css('-ms-filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + obj.url + '", sizingMethod="scale")');
+	$('.flickr').css('background-repeat', 'no-repeat');
+	$('.flickr').css('background-position', 'center center');
+	$('.flickr').css('background-attachment', 'fixed');
+}
+
+window.console||(window.console={log:function(){}});
+
+// Load Handlebars templates and json data
+var sectionTemplatePath = 'templates/section.handlebars';
+var dataPath = 'data/portfolio.json';
+
+$.when (
+	$.ajax ( sectionTemplatePath ),
+	$.getJSON ( dataPath ),
+	$(document).ready()
+).then( onDataLoad, onDataFail );
+
+function onDataLoad ( section, data ) {
+	var sectionHTML = section[0];
+	var sectionTemplate  = Handlebars.compile(sectionHTML);
+	var jsonData = data[0];
+
+	var html = $(sectionTemplate(jsonData));
+	$('.maincontent').append(html);
+}
+
+function onDataFail ( error ) {
+	$('.maincontent').html('<h1 class="error">There was an error loading site data.</h1>')
 }
