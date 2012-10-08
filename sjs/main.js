86633d97f61e697fe2668e442d946e50574ea87f
James Tomasino <james@tomasino.org>
2012-10-08 12:23:17 -0400

    restructured js with preload.js loading before content

--- sjs/main.js
+++ sjs/main.js
@@ -1,15 +1,14 @@
-//= require lib/modernizr
 //= require lib/jquery-1.8.2
 //= require lib/l10n
-//= require lib/appcache
-//= require flickr
+//= require lib/flickr
 
-var appCacheMediator = new AppCacheMediator();
 var flickr = new Flickr();
 flickr.showRandomPicture( onFlickrImage );
 
 function onFlickrImage ( obj ){
 	$('.background_image').css('background-image', 'url(' + obj.url + ')');
+	$('.background_image').css('filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + obj.url + '", sizingMethod="scale")');
+	$('.background-image').css('-ms-filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + obj.url + '", sizingMethod="scale")');
 	$('.background_image').css('background-repeat', 'no-repeat');
 	$('.background_image').css('background-position', 'center center');
 	$('.background_image').css('background-attachment', 'fixed');
