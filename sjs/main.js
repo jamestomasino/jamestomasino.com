bf43da33ab698f4654c05853f42addaa39a88a87
James Tomasino <james@tomasino.org>
2012-10-16 16:22:23 -0400

    removed all the flickr nonsense and useless chrome

--- sjs/main.js
+++ sjs/main.js
@@ -1,19 +1,5 @@
 //= require lib/jquery-1.8.2
 //= require lib/handlebars-1.0.rc.1
-//= require lib/l10n
-//= require lib/flickr
-
-var flickr = new Flickr();
-flickr.showRandomPicture( onFlickrImage );
-
-function onFlickrImage ( obj ){
-	$('.flickr').css('background-image', 'url(' + obj.url + ')');
-	$('.flickr').css('filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + obj.url + '", sizingMethod="scale")');
-	$('.flickr').css('-ms-filter: "progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + obj.url + '", sizingMethod="scale")');
-	$('.flickr').css('background-repeat', 'no-repeat');
-	$('.flickr').css('background-position', 'center center');
-	$('.flickr').css('background-attachment', 'fixed');
-}
 
 window.console||(window.console={log:function(){}});
 
