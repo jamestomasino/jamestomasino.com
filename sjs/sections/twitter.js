39574a5445c0d199f002d2d37f0001e423c1568a
James Tomasino <james@tomasino.org>
2012-10-19 15:12:27 -0400

    twitter functional and styled

--- sjs/sections/twitter.js
+++ sjs/sections/twitter.js
@@ -1,8 +1,8 @@
 //= require lib/chirp
 
 var twitterTemplate = {
-	base:'{{tweets}}',
-	tweet: '<p class="tweet">{{html}} - <span class="meta"><time><a href="http://twitter.com/{{user.screen_name}}/statuses/{{id_str}}">{{time_ago}}</a></time></span></p>'
+	base:'<ul>{{tweets}}</ul>',
+	tweet: '<li class="tweet"><div class="tweet-prefix">&ldquo;</div><span class="tweet-body">{{html}}</span><span class="tweet-meta"><time><a href="http://twitter.com/{{user.screen_name}}/statuses/{{id_str}}">{{time_ago}}</a></time></span></li>'
 }
 
 
@@ -12,7 +12,7 @@ Chirp({
   count: 100, //Total tweets to retrieve
   retweets: true, //Show/Don't show retweets
   replies: false,  //Show/Don't show replies
-  target: "twitter", //Target the id "twitter"
+  target: "twitter-content", //Target the id "twitter"
   templates: twitterTemplate, // Use custom template
   cacheExpire: 1000 * 60 * 60 //Number of milliseconds to cache tweets
 })
