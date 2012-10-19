0fa0b562eef681546ceeebe708a38270a3d5fcde
James Tomasino <james@tomasino.org>
2012-10-19 13:52:17 -0400

    moved js files into proper includes to get ready for twitter integration

---
+++ sjs/sections/portfolio.js
@@ -0,0 +1,24 @@
+//= require lib/jquery-1.8.2
+//= require lib/handlebars-1.0.rc.1
+// Load Handlebars templates and json data
+var sectionTemplatePath = 'templates/section.handlebars';
+var dataPath = 'data/portfolio.json';
+
+$.when (
+	$.ajax ( sectionTemplatePath ),
+	$.getJSON ( dataPath ),
+	$(document).ready()
+).then( onDataLoad, onDataFail );
+
+function onDataLoad ( section, data ) {
+	var sectionHTML = section[0];
+	var sectionTemplate  = Handlebars.compile(sectionHTML);
+	var jsonData = data[0];
+
+	var html = $(sectionTemplate(jsonData));
+	$('.portfolio').append(html);
+}
+
+function onDataFail ( error ) {
+	$('.portfolio').html('<section id="error" class="work"> <article> <header> <h1>There was an error loading the data.</h1> </header> </article> </section> ');
+}
