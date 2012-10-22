46870dd2747d91fa03fea79dc68af550196142d8
James Tomasino <james@tomasino.org>
2012-10-22 12:53:04 -0400

    wrapping section code in self-executing functions for clean global space

--- sjs/sections/portfolio.js
+++ sjs/sections/portfolio.js
@@ -1,25 +1,31 @@
 //= require ../lib/jquery-1.8.2
 //= require ../lib/handlebars-1.0.rc.1
 
-// Load Handlebars templates and json data
-var sectionTemplatePath = 'templates/section.handlebars';
-var dataPath = 'data/portfolio.json';
+(function(window, document, $, Handlebars) {
+	"use strict";
 
-$.when (
-	$.ajax ( sectionTemplatePath ),
-	$.getJSON ( dataPath ),
-	$(document).ready()
-).then( onDataLoad, onDataFail );
+	// Load Handlebars templates and json data
+	var sectionTemplatePath = 'templates/section.handlebars';
+	var dataPath = 'data/portfolio.json';
 
-function onDataLoad ( section, data ) {
-	var sectionHTML = section[0];
-	var sectionTemplate  = Handlebars.compile(sectionHTML);
-	var jsonData = data[0];
+	$.when (
+		$.ajax ( sectionTemplatePath ),
+		$.getJSON ( dataPath ),
+		$(document).ready()
+	).then( onDataLoad, onDataFail );
 
-	var html = $(sectionTemplate(jsonData));
-	$('.portfolio').append(html);
-}
+	function onDataLoad ( section, data ) {
+		var sectionHTML = section[0];
+		var sectionTemplate  = Handlebars.compile(sectionHTML);
+		var jsonData = data[0];
 
-function onDataFail ( error ) {
-	$('.portfolio').html('<section id="error" class="work"> <article> <header> <h1>There was an error loading the portfolio data.</h1> </header> </article> </section> ');
-}
+		var html = $(sectionTemplate(jsonData));
+		$('.portfolio').append(html);
+	}
+
+	function onDataFail ( error ) {
+		$('.portfolio').html('<section id="error" class="work"> <article> <header> <h1>There was an error loading the portfolio data.</h1> </header> </article> </section> ');
+	}
+
+}(window, document, jQuery, Handlebars));
+
