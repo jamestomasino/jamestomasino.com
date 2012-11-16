21aba7569c41f23255cbbb3dc08c63cb62c62132
James Tomasino <james@tomasino.org>
2012-11-15 20:14:10 -0500

    trying new jsonp

--- sjs/sections/github.js
+++ sjs/sections/github.js
@@ -6,14 +6,17 @@
 	"use strict";
 
 	var githubTemplatePath = 'templates/github.handlebars';
-	var github_api_repos = 'https://api.github.com/users/jamestomasino/repos?type=owner&sort=pushed&callback=?';
+	var github_api_repos = 'https://api.github.com/users/jamestomasino/repos?type=owner&sort=pushed';
 		// name property = repo name for commits query
 	var github_api_commits = 'https://api.github.com/repos/jamestomasino/sass-boilerplate/commits';
 	var githubChart;
 
 	$.when (
 		$.ajax ( githubTemplatePath ),
-		$.getJSON ( github_api_repos ),
+		$.ajax ( { type: 'GET',
+			url: github_api_repos,
+			dataType: 'jsonp'
+		}),
 		$(document).ready()
 	).then( onRepos, onDataFail );
 
@@ -21,7 +24,8 @@
 		var repoHTML = repoHandlebars[0];
 		var repoTemplate = Handlebars.compile(repoHTML);
 
-		var repoJSON = repoData[0];
+		var repoJSON = repoData[0].data;
+		console.log ( repoJSON );
 
 		for ( var i = 0; i < Math.min(10, repoJSON.length); ++i ) {
 			var repo = $(repoTemplate(repoJSON[i]));
