f12a8bfbec408113afadb1a7a5922f54114bda0c
James Tomasino <james@tomasino.org>
2013-06-07 17:05:28 -0400

    updating libraries

--- sjs/libs.js
+++ sjs/libs.js
@@ -1,9 +1,8 @@
-//= require ./lib/jquery-1.8.2
+//= require ./lib/jquery-1.10.1.min
 //= require ./lib/raphael-min
 //= require ./lib/g.raphael-min
 //= require ./lib/g.pie-min
 //= require ./lib/store
 //= require ./lib/enquire
-//= require ./lib/chirp
 //= require ./lib/highcharts
-//= require ./lib/handlebars-1.0.rc.1
+//= require ./lib/handlebars
--- sjs/main.js
+++ sjs/main.js
@@ -1,6 +1,5 @@
 //= require sections/portfolio
 //= require sections/github
-//= require sections/twitter
 //= require sections/coderbits
 //= require sections/analytics
 
