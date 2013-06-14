09aa0a75ff54664237d828265dd08d85fe5dd253
James Tomasino <james@tomasino.org>
2013-06-14 17:04:26 -0400

    adding back in some twitter code

--- sjs/main.js
+++ sjs/main.js
@@ -1,6 +1,8 @@
 //= require sections/portfolio
 //= require sections/github
 //= require sections/coderbits
+//= require sections/twitter
 //= require sections/analytics
 
 var coderbits = new Coderbits( '#coderbits' );
+//var twitter = new Twitter ( '#twitter-content', '345536786895884290');
