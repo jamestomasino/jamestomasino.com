937e5776126ccce35909b4d4223a3072eaf45cfa
James Tomasino <james@tomasino.org>
2013-06-15 17:38:48 -0400

    pulling json data for github activity and converting it to proper formats

--- sjs/main.js
+++ sjs/main.js
@@ -4,7 +4,7 @@
 //= require sections/analytics
 
 var coderbits = new Coderbits( '#coderbits-chart', '.coderbits' );
-var github = new Github ( '#github-content', '.github' );
+var github = new Github ( '#github-content', '#github-graph', '.github' );
 var twitter = new Twitter ( '#twitter-content', '345536786895884290', '.twitter');
 var analytics = new Analytics( 'UA-7902638-1' );
 
