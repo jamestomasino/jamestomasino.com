1165fc990adbc93d615d23003b4a95c7f2c87a91
James Tomasino <james@tomasino.org>
2013-06-15 15:41:40 -0400

    added twitter section back in with new code

--- sjs/main.js
+++ sjs/main.js
@@ -3,8 +3,8 @@
 //= require sections/twitter
 //= require sections/analytics
 
-var coderbits = new Coderbits( '#coderbits' );
+var coderbits = new Coderbits( '#coderbits-chart' );
 //var github = new Github ( '#github' );
-//var twitter = new Twitter ( '#twitter-content', '345536786895884290');
+var twitter = new Twitter ( '#twitter-content', '345536786895884290', '.twitter');
 var analytics = new Analytics( 'UA-7902638-1' );
 
