1833a1d3df7ef8f293c5996f2b0f914fc9558635
James Tomasino <james@tomasino.org>
2013-06-16 13:04:05 -0400

    added linkedin images and made it into a class

--- sjs/main.js
+++ sjs/main.js
@@ -7,5 +7,5 @@
 var coderbits = new Coderbits( '#coderbits-chart', '.coderbits' );
 var github = new Github ( '#github-content', '#github-graph', '.github' );
 var twitter = new Twitter ( '#twitter-content', '345536786895884290', '.twitter');
+var linkedin = new LinkedIn ( '#linkedin-content', '.linkedin');
 var analytics = new Analytics( 'UA-7902638-1' );
-
