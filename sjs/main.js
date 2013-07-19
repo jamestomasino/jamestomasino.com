da97a9b9a93887968b389d7f9e917111fb99ae9c
James Tomasino <james@tomasino.org>
2013-07-19 14:47:38 -0400

    commented out enquire tests in mainjs

--- sjs/main.js
+++ sjs/main.js
@@ -10,21 +10,21 @@ var twitter = new Twitter ( '#twitter-content', '345536786895884290', '.twitter'
 var linkedin = new LinkedIn ( '#linkedin-content', '.linkedin');
 var analytics = new Analytics( 'UA-7902638-1' );
 
-enquire2.register( { "max-width": 597 }, {
-	match : function() {
-		console.log ('1');
-	}
-});
+//enquire2.register( { "max-width": 597 }, {
+	//match : function() {
+		//console.log ('1');
+	//}
+//});
 
-enquire2.register( { "max-width": 887, "min-width":598 }, {
-	match : function() {
-		console.log ('2');
-	}
-});
+//enquire2.register( { "max-width": 887, "min-width":598 }, {
+	//match : function() {
+		//console.log ('2');
+	//}
+//});
 
-enquire2.register( { "min-width": 888 }, {
-	match : function() {
-		console.log ('3');
-	}
-});
+//enquire2.register( { "min-width": 888 }, {
+	//match : function() {
+		//console.log ('3');
+	//}
+//});
 
