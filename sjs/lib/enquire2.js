89c505744b633186e50cfc3b72d429db43f13769
James Tomasino <james@tomasino.org>
2013-07-19 11:52:35 -0400

    further shenanigans with enquire2

--- sjs/lib/enquire2.js
+++ sjs/lib/enquire2.js
@@ -5,15 +5,29 @@ window.enquire2 = window.enquire2 || ( function( window, document, matchMedia, u
 	var bool;
 
 	// Add more options to increase library features
-	var options = {
+	var globalOptions = {
 		'device': 'screen',
 		'min-width': undefined,
 		'max-width': undefined
 	}
 
-	return function( opt ){
+	var clone = function (obj) {
+		// Handle the 3 simple types, and null or undefined
+		if (null == obj || "object" != typeof obj) return obj;
+		// Handle Object
+		if (obj instanceof Object) {
+			var copy = {};
+			for (var attr in obj) {
+				if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+			}
+			return copy;
+		}
+	}
 
+	var register = function (opt, handlers) {
 		// override defaults
+		var options = clone(globalOptions);
+
 		for (var s in options) {
 			if ( opt.hasOwnProperty(s) ) options[s] = opt[s];
 		}
@@ -35,9 +49,53 @@ window.enquire2 = window.enquire2 || ( function( window, document, matchMedia, u
 			query += ' and (max-width: ' + options['max-width'] + ')';
 		}
 
-		return matchMedia(query);
+		return new MediaQuery(query, handlers);
+	}
 
-	};
+	function MediaQuery ( query, handlers ) {
+		var self = this;
+
+		self.query = query;
+		self.handlers = handlers;
+		self.mql = matchMedia(query);
+		self.matched = false;
+
+		self.listener = function (mql) {
+			self.mql = mql;
+			self.assess();
+		}
+
+		self.mql.addListener(self.listener);
+
+		return self.mql;
+	}
+
+	MediaQuery.prototype = {
+
+		assess : function () {
+
+			console.log (self);
+			var matches = self.mql.matches;
+
+			if ( matches && !self.matched ) {
+				if ( self.handlers.hasOwnProperty('match') ) {
+					self.matched = true;
+					self.handlers.match();
+				}
+			} else if ( !matches && self.matched ) {
+				if ( self.handlers.hasOwnProperty('unmatch') ) {
+					self.matched = false;
+					self.handlers.unmatch();
+				}
+			}
+		}
+	}
+
+
+
+	return {
+		register: register
+	}
 
 })( window, document, matchMedia );
 
--- sjs/main.js
+++ sjs/main.js
@@ -4,8 +4,27 @@
 //= require sections/linkedin
 //= require sections/analytics
 
-var coderbits = new Coderbits( '#coderbits-chart', '.coderbits' );
+//var coderbits = new Coderbits( '#coderbits-chart', '.coderbits' );
 var github = new Github ( '#github-content', '#github-graph', '.github' );
 var twitter = new Twitter ( '#twitter-content', '345536786895884290', '.twitter');
 var linkedin = new LinkedIn ( '#linkedin-content', '.linkedin');
 var analytics = new Analytics( 'UA-7902638-1' );
+
+enquire2.register( { "max-width": 597 }, {
+	match : function() {
+		console.log ('1');
+	}
+});
+
+enquire2.register( { "max-width": 887, "min-width":598 }, {
+	match : function() {
+		console.log ('2');
+	}
+});
+
+enquire2.register( { "max-width": 888 }, {
+	match : function() {
+		console.log ('3');
+	}
+});
+
