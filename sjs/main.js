39387dc0aec494fb51b06870abba3a6e41d62200
James Tomasino <james@tomasino.org>
2013-07-22 14:49:30 -0400

    adding back in coderbits and fixing twitter for old ie

--- sjs/main.js
+++ sjs/main.js
@@ -4,7 +4,7 @@
 //= require sections/linkedin
 //= require sections/analytics
 
-//var coderbits = new Coderbits( '#coderbits-chart', '.coderbits' );
+var coderbits = new Coderbits( '#coderbits-chart', '.coderbits' );
 var github = new Github ( '#github-content', '#github-graph', '.github' );
 var twitter = new Twitter ( '#twitter-content', '345536786895884290', '.twitter');
 var linkedin = new LinkedIn ( '#linkedin-content', '.linkedin');
