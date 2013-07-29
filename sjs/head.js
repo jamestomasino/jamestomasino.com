8f078102986f4da5dbec2f9834234cdc491b8958
James Tomasino <james@tomasino.org>
2013-07-29 13:38:50 -0400

    moving back to enquire with better polyfill

--- sjs/head.js
+++ sjs/head.js
@@ -1,7 +1,7 @@
 //= require lib/modernizr
 
 Modernizr.load([
-	{ test: window.matchMedia, nope: ['js/polyfill/respond.js', 'js/polyfill/enquire2-polyfill.js'] },
+	{ test: window.matchMedia, nope: ['js/polyfill/respond.js', 'js/polyfill/media.match.js'] },
 	{ test: Modernizr.svg, nope: 'js/polyfill/raphael.vml.js' },
 	"js/libs.js",
 	"js/main.js"
